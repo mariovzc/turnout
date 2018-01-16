8f961eddc8d91235706bed02f6a283346b70ea7d
Mario vizcaino <mario.vizcainos@gmail.com>
2018-01-16 09:43:15 -0500

    added submit event

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -2,17 +2,24 @@
   <div>
     <h3>Events DashBoard</h3>
     <button class="btn btn-danger btn-sm signout-btn" @click="signOut">Sign Out</button>
+    <hr>
+    <add-event></add-event>
+    <hr>
     {{$store.state}}
   </div> 
 </template>
 <script>
-import firebaseApp  from 'firebase'
+import {firebaseApp}  from '../firebaseApp'
+import AddEvent from './AddEvent'
 export default {
  methods: {
    signOut () {
      this.$store.dispatch('SIGN_OUT')
      firebaseApp.auth().signOut()
    }
- } 
+ },
+ components: {
+   AddEvent
+ }
 }
 </script>
--- src/firebaseApp.js
+++ src/firebaseApp.js
@@ -1,4 +1,4 @@
 import firebase from 'firebase'
 import config from './secret.json'
-
 export const firebaseApp = firebase.initializeApp(config)
+export const eventsRef = firebaseApp.database().ref().child('events')
